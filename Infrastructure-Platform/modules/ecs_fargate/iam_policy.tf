5041baa3c1edacf5ed69a0528ae89d432c1a3bd5
Ravi Senevirathne <ravi.senevirathne@contino.io@Ravis-MacBook-Pro.local>
2022-03-08 13:55:10 +1100

    update

--- Infrastructure-Platform/modules/ecs_fargate/iam_policy.tf
+++ Infrastructure-Platform/modules/ecs_fargate/iam_policy.tf
@@ -9,10 +9,18 @@ policy = jsonencode(
         {
             "Effect": "Allow",
             "Action": [
+                "ecr:GetAuthorizationToken",
                 "ecr:BatchCheckLayerAvailability",
-                "ecr:BatchGetImage",
                 "ecr:GetDownloadUrlForLayer",
-                "ecr:GetAuthorizationToken"
+                "ecr:GetRepositoryPolicy",
+                "ecr:DescribeRepositories",
+                "ecr:ListImages",
+                "ecr:DescribeImages",
+                "ecr:BatchGetImage",
+                "ecr:GetLifecyclePolicy",
+                "ecr:GetLifecyclePolicyPreview",
+                "ecr:ListTagsForResource",
+                "ecr:DescribeImageScanFindings"
             ],
             "Resource": "*"
         }
