008606e2c25a8222df00cbfce663a8c9c3610819
Ravi Senevirathne <ravi.senevirathne@contino.io@Ravis-MacBook-Pro.local>
2022-03-09 18:56:30 +1100

    App updated

--- Weather-Application/src/app.js
+++ Weather-Application/src/app.js
@@ -22,7 +22,7 @@ app.use(express.static(publicDirectoryPath));
 
 app.get("", (req, res) => {
   res.render("index", {
-    title: "Weather New 4",
+    title: "Weather New 5",
     name: "Phatthara Pisootrapee"
   });
 });
