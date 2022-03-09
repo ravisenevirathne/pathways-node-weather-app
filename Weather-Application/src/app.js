3819431845e65dcbf7f759f36615ae1f2fa2f7cf
Ravi Senevirathne <ravi.senevirathne@contino.io@Ravis-MacBook-Pro.local>
2022-03-09 23:01:17 +1100

    App heading updated

--- Weather-Application/src/app.js
+++ Weather-Application/src/app.js
@@ -22,7 +22,7 @@ app.use(express.static(publicDirectoryPath));
 
 app.get("", (req, res) => {
   res.render("index", {
-    title: "Weather New 4.3",
+    title: "Weather App Dojo",
     name: "Phatthara Pisootrapee"
   });
 });
